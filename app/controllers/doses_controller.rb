f9c857d7a1402e69dfdc6b2a8bc8b0ddd43845d9
karin knutsson <karinmiriam@gmail.com>
2021-01-19 19:29:09 +0100

    added actions in controllers for dose and cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,23 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.save
+    redirect_to cocktail_path
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
 end
