8648cb5384b1a9e3bdea8574a5c0ea9a67a51dd1
karin knutsson <karinmiriam@gmail.com>
2021-01-21 19:56:25 +0100

    added doses and ingredients to show page

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,13 @@
 class DosesController < ApplicationController
   def new
     @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
     @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     @dose.save
     redirect_to cocktail_path(@dose.cocktail)
   end
